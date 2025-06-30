21f60109d86be182ae461a82741e15c3a1f9041e
Ajay <ajay58singh@gmail.com>
2025-06-29 20:28:04 -0400

    updated test.py file

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Everyone!! This is used to create my app for understanding the gtihub actions."
-
+    return "Hello Everyone!!"
 
 if __name__=="__main__":
     app.run(host="0.0.0.0",port = 8000)
--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ def test_home():
     response = app.test_client().get("/")
 
     assert response.status_code==200
-    assert response.data== "Hello Everyone!! This is used to create my app for understanding the gtihub actions."
+    assert response.data=="Hello Everyone!!"
